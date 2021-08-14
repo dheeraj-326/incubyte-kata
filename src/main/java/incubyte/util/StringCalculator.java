6e0ba93cc66d7fe9ca638662598a201b6dacfeda
dheeraj-outwin-at-271334197815 <dheeraj.326@gmail.com>
2021-08-14 21:06:26 +0530

    Modifying to accommodate multiple delimiters

--- src/main/java/incubyte/util/StringCalculator.java
+++ src/main/java/incubyte/util/StringCalculator.java
@@ -2,21 +2,27 @@ package incubyte.util;
 
 import incubyte.exception.InvalidAdditionInputException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StringCalculator {
+
+    private final String[] delimiters = new String[] { ",", "\n" };
+
     public int Add(String numbers) throws InvalidAdditionInputException {
         int sum = 0;
         if (numbers == null)
             throw new InvalidAdditionInputException("InvalidInput: null");
         if (numbers.isEmpty())
             sum = 0;
-        else if (!numbers.contains(",")) {
+        else if (!containsDelimiter(numbers)) {
             try {
                 sum = Integer.parseInt(numbers);
             } catch (NumberFormatException ne) {
                 throw new InvalidAdditionInputException("InvalidInput: Not a number");
             }
         } else {
-            String[] parts = numbers.split(",");
+            ArrayList<String> parts = splitByDelimiter(numbers);
             try {
                 for (String part : parts) {
                     sum += Integer.parseInt(part);
@@ -26,5 +32,25 @@ public class StringCalculator {
             }
         }
         return sum;
+    }
+
+    private ArrayList<String> splitByDelimiter(String input) {
+        ArrayList<String> numbers = new ArrayList<>();
+        StringBuilder number = new StringBuilder("");
+        for (char character : input.toCharArray()) {
+            if (Arrays.stream(delimiters).anyMatch(delimiter -> Character.toString(character).equals(delimiter))) {
+                numbers.add(number.toString());
+                number = new StringBuilder("");
+            }
+        }
+        numbers.add(number.toString());
+        return numbers;
+    }
+
+    private boolean containsDelimiter(String input) {
+        for (String delimiter : delimiters)
+            if (input.contains(delimiter))
+                return true;
+        return false;
     }
 }
